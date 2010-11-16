8b911fc9e37336f13d0f76aac1f8e327fccd4767
Jeremy Banks <jeremy@jeremybanks.com>
2010-11-16 11:05:07 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject hehvolution "0.0.1-SNAPSHOT"
+  :description "Playing with evolving creatures in Clojure."
+  :dependencies [[clojure "1.2.0"]
+                 [clojure-contrib "1.2.0"]])
