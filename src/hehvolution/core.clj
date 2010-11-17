5def760ec96d6e5226c5b961ca23f6dc9571fc29
Jeremy Banks <jeremy@jeremybanks.com>
2010-11-16 21:20:46 -0500

    tiny little animation, yay it lives.

--- src/hehvolution/core.clj
+++ src/hehvolution/core.clj
@@ -2,3 +2,10 @@
 
 (defmacro def- [symbol value]
   `(def ^{:private true} ~symbol ~value)); TODO: not ignore existing meta
+
+(defn simulation []
+  {:state (ref {:guys [
+      {:x 10 :y 20}
+      {:x 30 :y 90}]})
+   :width 120
+   :height 120})
--- src/hehvolution/gui.clj
+++ src/hehvolution/gui.clj
@@ -1,27 +1,55 @@
 (ns hehvolution.gui
     (:require [hehvolution.core :as core])
-    (:import (javax.swing JFrame JPanel)
-             (java.awt Color Graphics2D Dimension)
+    (:import (java.lang Thread)
+             (javax.swing JFrame JPanel)
+             (java.awt Color Graphics2D Dimension BasicStroke)
              (java.awt.geom RoundRectangle2D$Double)))
 
 (defn rect
-  ([x y s] (rect x y s s 0 0))
-  ([x y w h] (rect x y w h 0 0))
-  ([x y w h arc] (rect x y w h arc arc))
+  ([s] (RoundRectangle2D$Double. 0 0 s s 0 0))
+  ([w h] (RoundRectangle2D$Double. 0 0 w h 0 0))
+  ([x y s] (RoundRectangle2D$Double. x y s s 0 0))
+  ([x y w h] (RoundRectangle2D$Double. x y w h 0 0))
+  ([x y w h arc] (RoundRectangle2D$Double. x y w h arc arc))
   ([x y w h arcw arch] (RoundRectangle2D$Double. x y w h arcw arch)))
 
-(defn open-window []
-  (let [window (JFrame.)
-        panel (proxy [JPanel] []
-          (paintComponent [g] (let [g2d (cast Graphics2D g)]
-            (proxy-super paintComponent g)
-            (.setColor g2d Color/red)
-            (.fill g2d (rect 5 5 50 50 10))
-            (.setColor g2d Color/black)
-            (.draw g2d (rect 5 5 50 50 10)))))]
-       (.setPreferredSize panel (Dimension. 300 300))
+(defn paint-guy [guy g2d scale]
+  (.setPaint g2d Color/red)
+  (.fill g2d (rect (* scale (guy :x)) (* scale (guy :y)) 8 8 3))
+  (.setPaint g2d Color/black)
+  (.draw g2d (rect (* scale (guy :x)) (* scale (guy :y)) 8 8 3)))
+
+; TODO: use agents and other idiomatic niceness
+(defn run-simulation [sim hertz]
+  (.start (Thread. (fn []
+    (while true
+      (Thread/sleep (* 1000 (/ 1 hertz)))
+      (dosync (alter (sim :state) (fn [old_state]
+        (assoc old_state :guys
+               (map (fn [old] (assoc old :x (+ 1 (old :x))))
+                    (old_state :guys)))))))))))
+
+(defn periodically-repaint [thing hertz]
+  (.start (Thread. (fn []
+    (while true
+      (Thread/sleep (* 1000 (/ 1 hertz)))
+      (.repaint thing))))))
+
+(defn open-window
+  ([] (open-window (core/simulation) 1))
+  ([sim scale]
+    (let [window (JFrame.)
+          panel (proxy [JPanel] []
+            (paintComponent [g] (let [g2d (cast Graphics2D g)]
+              (proxy-super paintComponent g)
+              (.setStroke g2d (BasicStroke. 2))
+              (doseq [guy (@(sim :state) :guys)] (paint-guy guy g2d scale)))))]
+       (.setPreferredSize panel (Dimension. (* (sim :width) scale)
+                                            (* (sim :height) scale)))
        (.add window panel)
        (.pack window)
        (.setDefaultCloseOperation window JFrame/EXIT_ON_CLOSE)               
        (.show window)
-       window))
+       (run-simulation sim 10)
+       (periodically-repaint panel 10)
+       window)))
