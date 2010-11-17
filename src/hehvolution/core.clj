95e79808bd23925dddf3dbd38160d56767327e01
Jeremy Banks <jeremy@jeremybanks.com>
2010-11-17 00:19:04 -0500

    ...

--- src/hehvolution/core.clj
+++ src/hehvolution/core.clj
@@ -1,11 +1,27 @@
 (ns hehvolution.core)
 
-(defmacro def- [symbol value]
+(defmacro def-
+  "Defines a private "
+  [symbol value]
   `(def ^{:private true} ~symbol ~value)); TODO: not ignore existing meta
 
-(defn simulation []
+(defn simulation
+  "Yeah!"
+  []
   {:state (ref {:guys [
       {:x 10 :y 20}
       {:x 30 :y 90}]})
-   :width 120
-   :height 120})
+    :width 120
+    :height 120})
+
+(defn advanced-state
+  "Returns what state becomes after a generation."
+  [state]
+    (assoc state :guys
+      (map (fn [guy] (assoc guy :x (+ 1 (guy :x))))
+           (state :guys))))
+
+(defn tick-sim
+  "Tick."
+  [sim]
+  (dosync (alter (sim :state) advanced-state)))
--- src/hehvolution/gui.clj
+++ src/hehvolution/gui.clj
@@ -3,39 +3,46 @@
     (:import (java.lang Thread)
              (javax.swing JFrame JPanel)
              (java.awt Color Graphics2D Dimension BasicStroke)
-             (java.awt.geom RoundRectangle2D$Double)))
+             (java.awt.geom RoundRectangle2D$Double Rectangle2D$Double)))
 
 (defn rect
-  ([s] (RoundRectangle2D$Double. 0 0 s s 0 0))
-  ([w h] (RoundRectangle2D$Double. 0 0 w h 0 0))
-  ([x y s] (RoundRectangle2D$Double. x y s s 0 0))
-  ([x y w h] (RoundRectangle2D$Double. x y w h 0 0))
+  "Creates a RectangularShape from [[x y] w] h [arcw [arch]]."
+  ([s] (Rectangle2D$Double. 0 0 s s))
+  ([w h] (Rectangle2D$Double. 0 0 w h))
+  ([x y s] (Rectangle2D$Double. x y s s))
+  ([x y w h] (Rectangle2D$Double. x y w h))
   ([x y w h arc] (RoundRectangle2D$Double. x y w h arc arc))
   ([x y w h arcw arch] (RoundRectangle2D$Double. x y w h arcw arch)))
 
-(defn paint-guy [guy g2d scale]
-  (.setPaint g2d Color/red)
-  (.fill g2d (rect (* scale (guy :x)) (* scale (guy :y)) 8 8 3))
-  (.setPaint g2d Color/black)
-  (.draw g2d (rect (* scale (guy :x)) (* scale (guy :y)) 8 8 3)))
+(defn paint-guy
+  "Paints a guy."
+  [guy g2d scale]
+    (.setPaint g2d Color/red)
+    (.fill g2d (rect (* scale (guy :x)) (* scale (guy :y)) 8 8 3))
+    (.setPaint g2d Color/black)
+    (.draw g2d (rect (* scale (guy :x)) (* scale (guy :y)) 8 8 3)))
+
+(defn paint-sim [sim scale])
 
 ; TODO: use agents and other idiomatic niceness
-(defn run-simulation [sim hertz]
-  (.start (Thread. (fn []
-    (while true
-      (Thread/sleep (* 1000 (/ 1 hertz)))
-      (dosync (alter (sim :state) (fn [old_state]
-        (assoc old_state :guys
-               (map (fn [old] (assoc old :x (+ 1 (old :x))))
-                    (old_state :guys)))))))))))
+(defn run-simulation
+  "Runs a simulation in a seperate thread."
+  [sim hertz]
+    (.start (Thread. (fn []
+      (while true
+        (Thread/sleep (* 1000 (/ 1 hertz)))
+        (core/tick-sim sim))))))
 
-(defn periodically-repaint [thing hertz]
-  (.start (Thread. (fn []
-    (while true
-      (Thread/sleep (* 1000 (/ 1 hertz)))
-      (.repaint thing))))))
+(defn periodically-repaint
+  "Yeah."
+  [thing hertz]
+    (.start (Thread. (fn []
+      (while true
+        (Thread/sleep (* 1000 (/ 1 hertz)))
+        (.repaint thing))))))
 
 (defn open-window
+  "Yoah."
   ([] (open-window (core/simulation) 1))
   ([sim scale]
     (let [window (JFrame.)
